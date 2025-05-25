9b967e3695b35490ddef2b547dc8311bf388123c
Rafi Sutrisno <mrafis2003@gmail.com>
2025-05-25 21:46:01 +0700

    add middleware route

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+const loginPath = '/login'
+
+const protectedRoutes = ['/dashboard', '/admin', '/student']
+
+export function middleware(request: NextRequest) {
+  console.log('middleware active')
+  const token = request.cookies.get('token')?.value
+
+  const { pathname } = request.nextUrl
+  console.log('ini pathname dan token:', pathname, token)
+
+  if (token && pathname === loginPath) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+
+  if (!token && protectedRoutes.some(route => pathname.startsWith(route))) {
+    return NextResponse.redirect(new URL(loginPath, request.url))
+  }
+
+  return NextResponse.next()
+}
+
+export const config = {
+  matcher: ['/login', '/dashboard/:path*', '/admin/:path*', '/student/:path*']
+}
