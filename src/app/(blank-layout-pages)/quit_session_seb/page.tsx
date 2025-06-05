f140c82b8bf41aa1e6161fc3722fd096310803b9
Rafi Sutrisno <mrafis2003@gmail.com>
2025-06-05 09:46:37 +0700

    add exam finish page and split view in exam student page

---
+++ src/app/(blank-layout-pages)/quit_session_seb/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useParams } from 'next/navigation'
+import ExamFeedback from '@/views/student/ExamFeedback'
+
+const ForgotPasswordPage = () => {
+  const params = useParams()
+  const id = params?.exam_id as string
+
+  return <>{id && <ExamFeedback id={id} />}</>
+}
+
+export default ForgotPasswordPage
--- src/app/(dashboard)/student/exam/[id]/page.tsx
+++ src/app/(dashboard)/student/exam/[id]/page.tsx
@@ -1,13 +1,25 @@
 'use client'
 
-import ViewExamStudent from '@/views/student/ViewExamStudent'
+import dynamic from 'next/dynamic'
+import type { ReactElement } from 'react'
+
+// import ViewExamStudent from '@/views/student/ViewExamStudent'
 import { useParams } from 'next/navigation'
+import SplitViewExamStudent from '@/views/student/split_views/ViewExamStudent'
+
+const ViewExamStudent = dynamic(() => import('@/views/student/ViewExamStudent'))
+const ResultTableStudent = dynamic(() => import('@/views/student/ViewResultStudent'))
+
+const tabContentList = (id: string): { [key: string]: ReactElement } => ({
+  exam: <ViewExamStudent id={id as string} />,
+  result: <ResultTableStudent id={id as string} />
+})
 
 const FormLayouts = () => {
   const params = useParams()
   const id = params?.id as string
 
-  return <>{id && <ViewExamStudent id={id as string} />}</>
+  return <SplitViewExamStudent tabContentList={tabContentList(id)} />
 }
 
 export default FormLayouts
