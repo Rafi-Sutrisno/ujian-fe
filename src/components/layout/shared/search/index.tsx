bc3d1b01040c6532e75d5e74f91e7597d4ac67b8
Rafi Sutrisno <mrafis2003@gmail.com>
2025-06-08 16:01:33 +0700

    remove search bar

--- src/components/layout/shared/search/index.tsx
+++ src/components/layout/shared/search/index.tsx
@@ -15,12 +15,13 @@ const NavSearch = () => {
       <i className='ri-search-line' />
     </IconButton>
   ) : (
-    <div className='flex items-center cursor-pointer gap-2'>
-      <IconButton className='text-textPrimary'>
-        <i className='ri-search-line' />
-      </IconButton>
-      <div className='whitespace-nowrap select-none text-textDisabled'>Search ⌘K</div>
-    </div>
+    // <div className='flex items-center cursor-pointer gap-2'>
+    //   <IconButton className='text-textPrimary'>
+    //     <i className='ri-search-line' />
+    //   </IconButton>
+    //   <div className='whitespace-nowrap select-none text-textDisabled'>Search ⌘K</div>
+    // </div>
+    <></>
   )
 }
 
