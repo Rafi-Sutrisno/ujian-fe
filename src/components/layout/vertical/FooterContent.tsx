5c580f687820c0b8a2355ba1e378b268e8ba5334
Rafi Sutrisno <mrafis2003@gmail.com>
2025-06-08 16:01:39 +0700

    remove footer

--- src/components/layout/vertical/FooterContent.tsx
+++ src/components/layout/vertical/FooterContent.tsx
@@ -25,22 +25,22 @@ const FooterContent = () => {
       </p>
       {!isBreakpointReached && (
         <div className='flex items-center gap-4'>
-          <Link href='https://themeselection.com/license' target='_blank' className='text-primary'>
+          {/* <Link href='https://themeselection.com/license' target='_blank' className='text-primary'>
             License
-          </Link>
+          </Link> */}
           {/* <Link href='https://themeselection.com' target='_blank' className='text-primary'>
             More Themes
           </Link>
           <Link href={process.env.NEXT_PUBLIC_DOCS_URL as string} target='_blank' className='text-primary'>
             Documentation
           </Link> */}
-          <Link
+          {/* <Link
             href={`https://github.com/themeselection/${process.env.NEXT_PUBLIC_REPO_NAME}/issues`}
             target='_blank'
             className='text-primary'
           >
             Support
-          </Link>
+          </Link> */}
         </div>
       )}
     </div>
