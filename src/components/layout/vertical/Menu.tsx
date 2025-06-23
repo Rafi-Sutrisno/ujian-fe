16955a25755338366dd5e06d531d37510aa0f844
Rafi Sutrisno <mrafis2003@gmail.com>
2025-06-23 21:49:11 +0700

    fix menu exact match false

--- src/components/layout/vertical/Menu.tsx
+++ src/components/layout/vertical/Menu.tsx
@@ -1,16 +1,18 @@
 'use client'
+
 import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 
 import { getRoleFromToken, getTokenFromCookies } from '@/utils/token'
 import { MenuItem, MenuSection } from '@menu/vertical-menu'
 
 const MenuAuthRole = () => {
+  const pathname = usePathname()
   const [role, setRole] = useState<'user' | 'admin' | null>(null)
 
   useEffect(() => {
     const token = getTokenFromCookies()
     const userRole = getRoleFromToken(token)
-
     setRole(userRole)
   }, [])
 
@@ -18,34 +20,61 @@ const MenuAuthRole = () => {
     <>
       {role === 'admin' && (
         <MenuSection label='Admin'>
-          <MenuItem href='/admin/user' icon={<i className='ri-group-line' />}>
+          <MenuItem
+            href='/admin/user'
+            icon={<i className='ri-group-line' />}
+            activeUrl='/admin/user'
+            exactMatch={false} // allows `/admin/user`, `/admin/user/123`, etc.
+          >
             User
           </MenuItem>
-          <MenuItem href='/admin/class' icon={<i className='ri-graduation-cap-line' />}>
+
+          <MenuItem
+            href='/admin/class'
+            icon={<i className='ri-graduation-cap-line' />}
+            activeUrl='/admin/class'
+            exactMatch={false} // only highlights on `/admin/class`
+          >
             Class
           </MenuItem>
-          <MenuItem href='/admin/exam' icon={<i className='ri-file-paper-2-line' />}>
+
+          <MenuItem
+            href='/admin/exam'
+            icon={<i className='ri-file-paper-2-line' />}
+            activeUrl='/admin/exam'
+            exactMatch={false}
+          >
             Exam
           </MenuItem>
-          <MenuItem href='/admin/problem' icon={<i className='ri-file-list-3-line' />}>
+
+          <MenuItem
+            href='/admin/problem'
+            icon={<i className='ri-file-list-3-line' />}
+            activeUrl='/admin/problem'
+            exactMatch={false}
+          >
             Problem
           </MenuItem>
         </MenuSection>
-
-        // <SubMenu label='Admin' icon={<i className='ri-home-smile-line' />}>
-        //   <MenuItem href='/'>Home</MenuItem>
-        //   <MenuItem href='/admin/user'>User</MenuItem>
-        //   <MenuItem href='/admin/class'>Class</MenuItem>
-        //   <MenuItem href='/admin/problem'>Problem</MenuItem>
-        // </SubMenu>
       )}
 
       {role === 'user' && (
         <MenuSection label='User'>
-          <MenuItem href='/user/class' icon={<i className='ri-graduation-cap-line' />}>
+          <MenuItem
+            href='/user/class'
+            icon={<i className='ri-graduation-cap-line' />}
+            activeUrl='/user/class'
+            exactMatch={false}
+          >
             Class
           </MenuItem>
-          <MenuItem href='/user/exam' icon={<i className='ri-file-list-3-line' />}>
+
+          <MenuItem
+            href='/user/exam'
+            icon={<i className='ri-file-list-3-line' />}
+            activeUrl='/user/exam'
+            exactMatch={false}
+          >
             Exam
           </MenuItem>
         </MenuSection>
